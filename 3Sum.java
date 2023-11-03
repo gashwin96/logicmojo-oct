1cbb8ebcf9d592b898e20dc75e1d8dce3a43e7b3
gashwin96 <63906808+gashwin96@users.noreply.github.com>
2023-11-03 20:01:15 +0530

    Added brute force for 3sum

--- 3Sum.java
+++ 3Sum.java
@@ -1,3 +1,31 @@
+//brute force
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i = 0; i<nums.length-2; i++) {
+                if(i!=0 && nums[i]==nums[i-1]) {
+                    continue;
+                }
+            for(int j = i+1; j<nums.length-1; j++) {
+                if(j-1>i && nums[j]==nums[j-1]) {
+                    continue;
+                }
+                for(int k=j+1; k<nums.length; k++) {
+                    if(nums[i] + nums[j] +nums[k] == 0) {
+                         res.add(new ArrayList<>(List.of(nums[i], nums[j], nums[k])));
+                        break;
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+}
+
+
+//optimal
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
